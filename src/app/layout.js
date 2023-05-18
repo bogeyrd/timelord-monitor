bed5335f0dbf5d02d4f0eb2796350c68129ca6a6
BHD Dev <dev@bhd.one>
2023-05-19 01:53:55 +0800

    Add `favicon.ico`.

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,7 +10,10 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang='en'>
+      <head>
+        <link rel='shortcut icon' href='/favicon.ico' />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
